1855f2b3ec8da4f55521120e0609c9871f567636
shubhams007 <31588572+shubhams007@users.noreply.github.com>
2017-09-03 17:20:13 +0530

    Add files via upload

---
+++ MyTest.java
@@ -0,0 +1,12 @@
+package com.identityiq;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String a="hi";
+     String b = "hi";
+     System.out.println(a+b);
+	}
+
+}
